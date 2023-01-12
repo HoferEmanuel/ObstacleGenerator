3a3da36fea6983ddb6e0cb60adabc05ad8731282
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2023-01-12 11:51:31 +0100

    new12012023w

--- ObjectPooler.cs
+++ ObjectPooler.cs
@@ -7,14 +7,37 @@ public class ObjectPooler : MonoBehaviour
 
     public Transform poolContent;
 
+    public List<ObjectPool> pools;
+
     private void Awake()
     {
         current = this;
+
+        GeneratePoolObjects();
     }
 
-    void CreatePoolObjects()
+    void GeneratePoolObjects()
     {
-        
+        foreach(ObjectPool pool in pools)
+        {
+            pool.CreatePoolContent;
+
+            foreach(PoolObject poolObj in pool.poolObjects)
+            {
+
+                GameObject currentPrefab = Instantiate();
+            }
+        }
+    }
+
+    void CreatePoolObject(GameObject targetObj, Vector3 targetPos, Vector3 targetRot, float sizeMultiply)
+    {
+        targetObj.SetActive(true);
+    }
+
+    void DisablePoolObject(GameObject targetObj)
+    {
+        targetObj.SetActive(false);
     }
 }
 
@@ -22,7 +45,14 @@ public class ObjectPooler : MonoBehaviour
 public class ObjectPool
 {
     public string tag;
-    public List<PoolObject> preafabs;
+    public List<PoolObject> poolObjects;
+    public Transform poolContent;
+
+    public void CreatePoolContent()
+    {
+        Transform content = new Transform();
+        poolContent = content;
+    }
 }
 
 [System.Serializable]
